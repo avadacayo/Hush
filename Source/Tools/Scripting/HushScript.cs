a86574f8c2d154b6f96e0c4fa377b57a9e2a23f4
Matt Jang <matt.hm.jang@gmail.com>
2014-06-03 20:19:21 -0400

    started scripting & imported Json.NET

---
+++ Source/Tools/Scripting/HushScript.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Jint;
+
+namespace Hush.Tools.Scripting
+{
+
+    class HushScript
+    {
+
+        private Engine _Engine = null;
+
+        public HushScript()
+        {
+
+            _Engine = new Engine();
+
+        }
+
+    }
+
+}

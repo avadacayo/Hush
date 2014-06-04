de1ebf68775d4aceab95f15effb33a4d4d5b9665
Matt Jang <matt.hm.jang@gmail.com>
2014-06-03 22:08:11 -0400

    added some script logic

---
+++ Source/Tools/FileUtil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hush.Tools
+{
+
+    static class FileUtil
+    {
+
+        public static String ReadDataFile(String FileName)
+        {
+            return String.Empty;
+        }
+
+        public static String ReadScriptFile(String FileName)
+        {
+            return String.Empty;
+        }
+
+        public static Boolean SaveDataFile(Object Data)
+        {
+            return false;
+        }
+
+    }
+
+}
--- Source/Tools/Scripting/HushScript.cs
+++ Source/Tools/Scripting/HushScript.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Jint;
+using Jint.Native;
+using Hush.Tools;
 
 namespace Hush.Tools.Scripting
 {
@@ -10,13 +12,53 @@ namespace Hush.Tools.Scripting
     class HushScript
     {
 
-        private Engine _Engine = null;
+        private JsValue _BodyFunction;
+        private Engine _Engine;
+        private JsValue _HeadFunction;
+        private Boolean _Loaded;
+        private String _Name;
+        private String _Source;
+
+        public String Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
 
         public HushScript()
         {
 
             _Engine = new Engine();
+            _Loaded = false;
+            _Name = String.Empty;
+            _Source = String.Empty;
 
+        }
+
+        public Boolean Load()
+        {
+            _Loaded = false;
+            if (_Name == String.Empty)
+                return false;
+            _Source = FileUtil.ReadScriptFile(_Name);
+            if (_Source.Length < 1)
+                return false;
+            _Engine.Execute(_Source);
+            _BodyFunction = _Engine.GetValue("body");
+            _HeadFunction = _Engine.GetValue("head");
+            if (_BodyFunction == JsValue.Undefined || _HeadFunction == JsValue.Undefined)
+                return false;
+            _Loaded = true;
+            return true;
+        }
+
+        public Boolean Run()
+        {
+            if (!_Loaded)
+                return false;
+            var x = _HeadFunction.Invoke();
+            var y = _BodyFunction.Invoke();
+            return true;
         }
 
     }

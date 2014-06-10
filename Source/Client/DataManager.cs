0473a8421defda8b228e555a1840d0a30b2f175a
Guiwen Chen <chch@live.ca>
2014-06-09 22:49:13 -0400

    updated dataManager

--- Source/Client/DataManager.cs
+++ Source/Client/DataManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.Serialization;
 using Hush.Client.Model;
 using System.Windows.Forms;
+using System.Data;
 
 namespace Hush.Client
 {
@@ -59,17 +60,33 @@ namespace Hush.Client
         }
 
         public static void AddRecord(Int32 RecordIndex, String Key, String Value) {
-            Record r = new Record();
-            Field f_k = new Field();
-            f_k.Key = Key;
-            f_k.Value = Value;
-            DataHolder.CurrentUser.Records[RecordIndex].Fields.Add(f_k);
+            Field NewField = new Field();
+            NewField.Key = Key;
+            NewField.Value = Value;
+            DataHolder.CurrentUser.Records[RecordIndex].Fields.Add(NewField);
 
             return;
         }
 
-        public static void EditRecord(object sender, DataGridViewCellEventArgs e)
+        public static void ApplyRecordChanges(Record CurrentRecord, DataGridView Data)
         {
+            DataTable DT;
+            DataView DV = (DataView)Data.DataSource;
+            DT = DV.Table.DataSet.Tables[0];
+            CurrentRecord.Fields.Clear();
+            
+            foreach ( DataRow Row in  DT.Rows) {
+                
+                    Field f = new Field();
+                    f.Key = Row[0].ToString();
+                    f.Value = Row[1].ToString();
+                    CurrentRecord.Fields.Add(f);
+                              
+            }
+         }
+
+        //public static void EditRecord(object sender, DataGridViewCellEventArgs e)
+        //{
 
             //var collection = this.dataGridViewRecords.Rows;
 
@@ -87,7 +104,7 @@ namespace Hush.Client
             //    }
             //}
 
-        }
+        //}
 
     }
 

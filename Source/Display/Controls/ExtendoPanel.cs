eee4bf416c9d765ada8613dad2bebaa556822da8
Matt Jang <matt.hm.jang@gmail.com>
2014-06-16 18:14:53 -0400

    script dialog work

---
+++ Source/Display/Controls/ExtendoPanel.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Forms;
+
+namespace Hush.Display.Controls
+{
+
+    class ExtendoPanel : Panel
+    {
+    }
+
+}
--- Source/Display/Interfaces/ScriptDialog.cs
+++ Source/Display/Interfaces/ScriptDialog.cs
@@ -2,11 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Hush.Display.Controls;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace Hush.Display.Interfaces
 {
+
     class ScriptDialog : Interface
     {
+
+        protected Boolean HasButtons;
+
+        #region Controls
+
+        protected Button BackButton;
+        protected Button CancelButton;
+        protected Button NextButton;
+        protected Button OkButton;
+        protected ExtendoPanel ContentPanel;
+
+        #endregion
+
+        public ScriptDialog()
+        {
+
+            HasButtons = false;
+
+        }
 
         #region Designer
 
@@ -15,6 +38,10 @@ namespace Hush.Display.Interfaces
 
             base.Initialize(Title);
             SuspendLayout();
+            ContentPanel = new ExtendoPanel();
+            ContentPanel.Location = new Point(10, 10);
+            Controls.Add(ContentPanel);
+            ClientSize = new Size(500, 200);
             ResumeLayout(true);
 
         }
@@ -22,4 +49,13 @@ namespace Hush.Display.Interfaces
         #endregion
 
     }
+
+    class ScriptDialogText : ScriptDialog
+    {
+    }
+
+    class ScriptDialogPrompt : ScriptDialog
+    {
+    }
+
 }

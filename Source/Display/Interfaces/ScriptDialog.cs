0184e760695ba50e8d271b1c786d33dc764b6bc9
Matt Jang <matt.hm.jang@gmail.com>
2014-06-16 18:09:10 -0400

    add ScriptDialog class

---
+++ Source/Display/Interfaces/ScriptDialog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hush.Display.Interfaces
+{
+    class ScriptDialog : Interface
+    {
+
+        #region Designer
+
+        protected override void Initialize(List<String> Title)
+        {
+
+            base.Initialize(Title);
+            SuspendLayout();
+            ResumeLayout(true);
+
+        }
+
+        #endregion
+
+    }
+}

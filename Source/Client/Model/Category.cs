6f2186cbf6b086f9a2618b16cdf10309c5efeae7
Matt Jang <matt.hm.jang@gmail.com>
2014-06-20 18:10:30 -0400

    cleaned up category class

--- Source/Client/Model/Category.cs
+++ Source/Client/Model/Category.cs
@@ -5,67 +5,109 @@ using System.Security.Permissions;
 
 namespace Hush.Client.Model
 {
+
     [Serializable]
     class Category : ISerializable
     {
+
+        private Category _Category;
+        private DateTime _Created;
         private String _ID;
         private String _Name;
         private List<Record> _Records;
-        private DateTime _Created;
 
+        #region Properties
 
-        public Category()
+        public Category Category
         {
-            //_ID = string.Empty;
-            //_Name = string.Empty;
-            //_Records = new List<Record>();
-            //_Created = DateTime.Now;
+
+            get { return _Category; }
+            set { _Category = value; }
+
         }
 
-        protected Category(SerializationInfo Info, StreamingContext context)
+        public DateTime Created
         {
-            if (Info == null)
-                throw new System.ArgumentNullException("Info");
 
-            _ID = (String)Info.GetValue("ID", typeof(String));
-            _Name = (String)Info.GetValue("Name", typeof(String));
-            _Records = (List<Record>)Info.GetValue("Record", typeof(List<Record>));
-            _Created = (DateTime)Info.GetValue("Created", typeof(DateTime));
+            get { return _Created; }
+            set { _Created = value; }
+
         }
 
         public String ID
         {
+
             get { return _ID; }
             set { _ID = value; }
+
         }
+
         public String Name
         {
+
             get { return _Name; }
             set { _Name = value; }
+
         }
+
         public List<Record> Records
         {
+
             get { return _Records; }
             set { _Records = value; }
+
         }
-        public DateTime Created
+
+        #endregion
+
+        public Category()
         {
-            get { return _Created; }
-            set { _Created = value; }
+
+            _Category = null;
+            _Created = DateTime.Now;
+            _ID = String.Empty;
+            _Name = String.Empty;
+            _Records = new List<Record>();
+
+        }
+
+        protected Category(SerializationInfo Info, StreamingContext context)
+        {
+
+            if (Info == null)
+            {
+
+                throw new System.ArgumentNullException("Info");
+
+            }
+
+            _Category = null;
+            _Created = (DateTime)Info.GetValue("Created", typeof(DateTime));
+            _ID = (String)Info.GetValue("ID", typeof(String));
+            _Name = (String)Info.GetValue("Name", typeof(String));
+            _Records = (List<Record>)Info.GetValue("Record", typeof(List<Record>));
+
         }
 
         [SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.SerializationFormatter)]
         public virtual void GetObjectData(SerializationInfo Info, StreamingContext Context)
         {
+
             if (Info == null)
             {
+
                 throw new ArgumentNullException("Info");
+
             }
 
+            Info.AddValue("Category", 0);
+            Info.AddValue("Created", _Created);
             Info.AddValue("ID", _ID);
             Info.AddValue("Name", _Name);
             Info.AddValue("Records", _Records);
-            Info.AddValue("Created", _Created);
+
         }
+
     }
+
 }

c48d744800c51a84c505ec7e40fa483a267c38b6
Matt Jang <matt.hm.jang@gmail.com>
2014-08-01 19:01:51 -0400

    cdot wiki script

--- Data/Templates/CDOT Wiki/Change Password.js
+++ Data/Templates/CDOT Wiki/Change Password.js
@@ -1,10 +1,13 @@
 ﻿var status = -1;
 
-var pages = {
-    form : "http://zenit.senecac.on.ca/wiki/index.php?title=Special:UserLogin",
-    action : "http://zenit.senecac.on.ca/wiki/index.php?title=Special:UserLogin&action=submitlogin&type=login&returnto=Main_Page",
-    pref: "http://zenit.senecac.on.ca/wiki/index.php/Special:Preferences",
-};
+var success = false;
+var current_page = "";
+var login_token = "";
+var login_page = "";
+var password_new = "";
+var password_old = "";
+var password_token = "";
+var username = "";
 
 function body(state, mode, value) {
 
@@ -18,6 +21,90 @@ function body(state, mode, value) {
     switch (status) {
 
         case 0:
+
+            WebHandler.ClearCookies();
+
+            username = AccessHandler.Access("username");
+            password_old = AccessHandler.Access("password");
+
+            login_page = WebHandler.SendGet(
+                "http://zenit.senecac.on.ca/wiki/index.php?title=Special:UserLogin"
+            );
+
+            login_token = get_login_token(login_page);
+
+            login_page = WebHandler.SendPost(
+                "http://zenit.senecac.on.ca/wiki/index.php?title=Special:UserLogin&action=submitlogin&type=login&returnto=Main_Page",
+                "wpName=" + encodeURIComponent(username).replace("%20", "+") +
+                "&wpPassword=" + encodeURIComponent(password_old) +
+                "&wpLoginattempt=Log+in" +
+                "&wpLoginToken=" + login_token
+            );
+
+            if (login_page.indexOf("var wgUserName = \"" + username + "\";") > 1) {
+
+                success = true;
+                ViewHandler.ShowInputDialog("new password:");
+
+            }
+
+            else {
+
+                ViewHandler.ShowTextDialog("Unable to login with current information.");
+                success = false;
+
+            }
+
+            break;
+
+        case 1:
+
+            password_new = value;
+
+            if (success == false) {
+
+                ViewHandler.Close();
+                return;
+
+            }
+
+            current_page = WebHandler.SendGet("http://zenit.senecac.on.ca/wiki/index.php?title=Special:ChangePassword&returnto=Special%3APreferences");
+            password_token = get_password_token(current_page);
+
+            WebHandler.SendPost(
+                "http://zenit.senecac.on.ca/wiki/index.php/Special:ChangePassword",
+                "token=" + encodeURIComponent(password_token) +
+                "&wpName=" + encodeURIComponent(username).replace("%20", "+") +
+                "&returnto=Special%3APreferences" +
+                "&wpPassword=" + encodeURIComponent(password_old) +
+                "&wpNewPassword=" + encodeURIComponent(password_new) +
+                "&wpRetype=" + encodeURIComponent(password_new)
+            );
+
+            login_page = WebHandler.SendGet(
+                "http://zenit.senecac.on.ca/wiki/index.php?title=Special:UserLogin"
+            );
+
+            login_token = get_login_token(login_page);
+
+            login_page = WebHandler.SendPost(
+                "http://zenit.senecac.on.ca/wiki/index.php?title=Special:UserLogin&action=submitlogin&type=login&returnto=Main_Page",
+                "wpName=" + encodeURIComponent(username).replace("%20", "+") +
+                "&wpPassword=" + encodeURIComponent(password_new) +
+                "&wpLoginattempt=Log+in" +
+                "&wpLoginToken=" + login_token
+            );
+
+            if (login_page.indexOf("var wgUserName = \"" + username + "\";") > 1) {
+
+                AccessHandler.Set("password", password_new);
+                ViewHandler.ShowTextDialog("Password changed!");
+                return;
+
+            }
+
+            ViewHandler.ShowTextDialog("Unable to change password.");
+
             break;
 
         default:
@@ -28,11 +115,23 @@ function body(state, mode, value) {
 
 }
 
-function getLoginToken() {
+function get_login_token(login_page) {
 
-    var page = WebHandler.SendGet(pages.form);
     var regex = /name="wpLoginToken" value="([^"]+)"/ig;
-    var result = regex.exec(page);
+    var result = regex.exec(login_page);
+
+    if (result.length >= 2) {
+        return result[1];
+    }
+
+    return false;
+
+}
+
+function get_password_token(login_page) {
+
+    var regex = /name="token" type="hidden" value="([^"]+)"/ig;
+    var result = regex.exec(login_page);
 
     if (result.length >= 2) {
         return result[1];
